e5d45dcae16fbc78949507ec7c4138e87c64ebbd
nurrohmatulfaizah <nurrohmatulfaizah45@gmail.com>
2021-11-25 18:30:36 +0700

    .

---
+++ application/controllers/latihan.php
@@ -0,0 +1,12 @@
+<?php
+class latihan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Nur Rohmatul Faizah
+        Saya tinggal di daerah Tangerang
+        Olahraga yang saya sukai adalah
+        Badminton";
+    }
+}
